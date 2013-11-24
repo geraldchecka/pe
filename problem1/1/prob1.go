f96c1a75c1cf54d9961555f8d0a15a12af5f837a
anvesh <anvesh@ubuntu.(none)>
2013-11-24 19:52:16 +0530

    Benchmarked Problem1

--- problem1/1/prob1.go
+++ problem1/1/prob1.go
@@ -1,10 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func problem1() int {
+func Problem1() int {
 	sum1, sum2, sum3 := 0, 0, 0
 	var i int = 1
 	for (5 * i) < 1000 {
@@ -24,6 +22,18 @@ func problem1() int {
 	return (sum1+sum2-sum3)
 }
 
+func Problem1V2() int {
+	var sum int = 0
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
 func main() {
-	fmt.Println("Sum of multiples of 3 or 5 less than 1000 is: ",problem1())
+	fmt.Println("Sum of multiples of 3 or 5 less than 1000");
+	fmt.Printf("Algorithm 1: %v\n", Problem1())
+	fmt.Printf("Algorithm 2: %v\n", Problem1V2())
 }
--- problem1/1/prob1_test.go
+++ problem1/1/prob1_test.go
@@ -5,8 +5,27 @@ import (
 )
 
 func TestProblem1(t *testing.T) {
-	result := problem1()
+	result := Problem1()
 	if result != 233168 {
-		t.Errorf("Expected answer: %v, obtained result: %v", 233168, result)
+		t.Errorf("Algorithm 1: Expected answer: %v, obtained result: %v\n", 233168, result)
+	}
+}
+
+func TestProblem1V2(t *testing.T) {
+	result2 := Problem1V2()
+	if result2 != 233168 {
+		t.Errorf("ALgorithm 2: Expected answer: %v, obtained result: %v\n", 233168, result2)
+	}
+}
+
+func BenchmarkAlgorithm1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Problem1()
+	}
+}
+
+func BenchmarkAlgorithm2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Problem1V2()
 	}
 }
