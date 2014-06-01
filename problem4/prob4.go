ec7f77ea8d75ec2751fdc9fbf06eeb9655b96108
anvesh <anvesh@ubuntu.(none)>
2014-06-01 22:23:54 +0530

    problem 4: partila

--- problem4/prob4.go
+++ problem4/prob4.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"unicode/utf8"
 )
 func LargestPalindrome() string {
 	for	i := 999; i > 99; i-- {
@@ -14,17 +15,23 @@ func LargestPalindrome() string {
 }
 
 func IsPalindrome(str string) bool {
-	makeString := make([]string, len(str))
-	return str == reverse(str)
+	return str == Reverse(str)
 }
 
 func Reverse(str string) string {
-	var revStr string;
-	for 
-	return revStr
+	revStrRune, rIndex := make([]rune, len(str), utf8.RuneCountInString(str)), 0
+	for _, rValue := range str {
+		revStrRune[rIndex] = rValue
+		rIndex++
+	}
+	for i, rCount := 0, utf8.RuneCountInString(str); i < rCount / 2; i++ {
+		revStrRune[i], revStrRune[rCount - 1 - i] = revStrRune[rCount - 1 - i], revStrRune[i]
+	}
+	return string(revStrRune)
 }
 
 func main() {
-	fmt.Println("Project Euler - 4\n")
-	fmt.Printf("The largest palindrome: %v\n",LargestPalindrome())
+	fmt.Println("Project Euler - 4")
+	//fmt.Printf("The largest palindrome: %v\n",LargestPalindrome())
+	fmt.Println(IsPalindrome("sahas"))
 }
