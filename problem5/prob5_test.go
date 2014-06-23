3fe82c9a70d2a9319efea3082afde997308e7023
anvesh <anvesh@ubuntu.(none)>
2014-06-23 06:35:10 +0530

    prob5: alg1: w/ benchmark

---
+++ problem5/prob5_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAlgorithm1 (t *testing.T) {
+	result1 := Algorithm1();
+	if result1 != 232792560 {
+		t.Errorf("Algorithm1: Expected result: %v, Obtained result: %v", 232792560, result1)
+	}
+}
+
+func BenchmarkAlgorithm1 (b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Algorithm1();
+	}
+}
