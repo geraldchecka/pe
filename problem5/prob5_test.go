3e53289c9404e0f08d6d07f35fcb3fc79d3f0f2b
anvesh <anvesh@ubuntu.(none)>
2014-07-10 08:50:27 +0530

    problem 6

--- problem5/prob5_test.go
+++ problem5/prob5_test.go
@@ -4,15 +4,15 @@ import (
 	"testing"
 )
 
-func TestAlgorithm1 (t *testing.T) {
-	result1 := Algorithm1();
+func TestAlgorithm1(t *testing.T) {
+	result1 = Algorithm1()
 	if result1 != 232792560 {
 		t.Errorf("Algorithm1: Expected result: %v, Obtained result: %v", 232792560, result1)
 	}
 }
 
-func BenchmarkAlgorithm1 (b *testing.B) {
+func BenchmarkAlgorithm1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Algorithm1();
+		Algorithm1()
 	}
 }
---
+++ problem6/prob6.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Algorithm1(n int) int {
+	return int(math.Pow(float64(n*(n+1)/2), 2) - float64(n*(n+1)*(2*n+1))/6)
+}
+
+func main() {
+	fmt.Printf("%v\n", Algorithm1(100))
+}
