df983af7954d37c6d98d38b10c0a1bec49760e2a
anvesh <anvesh@ubuntu.(none)>
2014-07-10 08:52:35 +0530

    prob6: updates

--- problem6/prob6.go
+++ problem6/prob6.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+//Use struct in this alg
+func Algorithm2() {
+
+}
+
 func Algorithm1(n int) int {
 	return int(math.Pow(float64(n*(n+1)/2), 2) - float64(n*(n+1)*(2*n+1))/6)
 }
