3def7a5d339b58b16a2a788cad22cac149d1c1bc
anvesh <anvesh@ubuntu.(none)>
2014-07-11 11:15:31 +0530

    problem 7 w/o benchmark

---
+++ problem7/prob7.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Algorithm1() int {
+	index, isPrime := 1, true
+	for i := 3; i <= i; i += 2 {
+		isPrime = true
+		for j := 2; j <= int(math.Sqrt(float64(i))); j++ {
+			if i%j == 0 {
+				isPrime = false
+				continue
+			}
+		}
+		if isPrime {
+			index++
+		}
+		if index == 10001 {
+			return i
+		}
+
+	}
+	return 0
+}
+func main() {
+	fmt.Printf("Hello Problem 7: %v", Algorithm1())
+}
---
+++ problem7/prob7_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAlgorithm1() {
+
+}
+
+func BenchmarkAlgorithm1() {
+
+}
